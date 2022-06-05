3df5867b1b77500ce3d1f13fe4570fc2c7997f0c
thetaru <sensesensesense012345678910@gmail.com>
2022-06-05 21:56:17 +0900

    A Tour of Go Basic - Slices of slices

---
+++ A-Tour-of-Go/02_Basics/35_slices-pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// スライスして作った配列に格納される要素はスライス元配列の要素のポインタ
+func main() {
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
+}
